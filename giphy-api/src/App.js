33411e6a27507707fb9d55350c35012d098a4a2e
Anastasiya Chabotska <anc33139@gmail.com>
2021-01-13 21:29:57 -0500

    search function and homepage with trending gifs working

--- giphy-api/src/App.js
+++ giphy-api/src/App.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Searchbar from './components/SearchBar'
-
-function App() {
-  return (
-    <div className="App">
-      <Searchbar />
-    </div>
-  );
-=======
 import React, { Component } from "react";
 import "./App.css";
 import GifCard from "./components/GifCard";
+import Searchbar from "./components/SearchBar";
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +10,16 @@ class App extends Component {
     this.state = {
       data: [],
     };
+
+    this.updateData = this.updateData.bind(this);
   }
+
+  //function to be called from SearchBar component to update the data state
+  updateData(newData) {
+    this.setState({ data: newData });
+  }
+
+  //display trending when component mounts
   componentDidMount() {
     fetch(
       `https://api.giphy.com/v1/gifs/trending?api_key=iCou32qYmtidVoNOAQl5QJtlpLVRNfzk`
@@ -30,21 +28,29 @@ class App extends Component {
       .then((response) => {
         this.setState({ data: response.data });
       })
+
       .catch((error) => console.error(error));
   }
 
   render() {
     console.log(this.state.data);
     return (
-      <div className="display">
-        {this.state.data.map((item, index) => (
-          <GifCard key={index} url={item.images.original.url} />
-        ))}
+      <div>
+        <div className="nav">
+          <Searchbar update={this.updateData} />
+        </div>
+        <div className="display">
+          {this.state.data.map((item, index) => (
+            <GifCard key={index} url={item.images.original.url} />
+          ))}
+        </div>
+
+        {/* {this.state.displayTrending ? this.trendingGifs : <p>nothing</p>} */}
+
         {/* <img src={this.state.data[0].embed_url} /> */}
       </div>
     );
   }
->>>>>>> trending-gifs
 }
 
 export default App;
